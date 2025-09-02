9b3d61b81d9682ec6946deacc78c55f2fce60132
Alex <levashov.av@phystech.edu>
2025-09-02 15:37:34 +0300

    git status!

---
+++ ConsoleApplication3/ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+int x, y;
+int N = 0;
+int M = 1;
+
+int main()
+{
+	cin >> x;
+	for (int M = 1; N < x; M++) {
+		while (M % y == 0) {
+			y += 1;
+			if (y == M) {
+				break;
+				N += 1;
+			if (N == x) {
+				cout << M;
+				}
+			}
+		}
+		
+
+	}
+	
+	
+
+}
